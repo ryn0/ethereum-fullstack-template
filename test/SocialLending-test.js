1aafea0a8543a305ee1b7193d9ededba32f0d46a
Ryan Williams <ryanmichaelwilliams@gmail.com>
2022-01-23 12:46:59 -0700

    reformat tests

--- test/SocialLending-test.js
+++ test/SocialLending-test.js
@@ -36,23 +36,23 @@ describe("SocialLending Contract", () => {
             SocialLendingContract.connect(owner).createLoan(0)
         ).to.be.revertedWith("Loan amount must be greater than zero.");
 
-      });
+    });
 
-      it("Should emit LoanRequested event when loan is created", async function () {
+    it("Should emit LoanRequested event when loan is created", async function () {
         await expect(
           SocialLendingContract.connect(sender).createLoan(1000)
         ).to.emit(SocialLendingContract, "LoanRequested")
         .withArgs(1);
-      });
+    });
 
-      it("Should should get back correct loan values for first loan if one is created", async function () {
+    it("Should should get back correct loan values for first loan if one is created", async function () {
         await SocialLendingContract.connect(sender).createLoan(1000);
         let loanDetails = await SocialLendingContract.loanDetails(1);
       
         expect(loanDetails.loanID).to.equal(1) && expect(loanDetails.loanAmount).to.equal(1000);
     });
 
-      it("Should should get back correct loan values for second loan if two are created", async function () {
+    it("Should should get back correct loan values for second loan if two are created", async function () {
         await SocialLendingContract.connect(sender).createLoan(1000);
         await SocialLendingContract.connect(sender).createLoan(10000);
         let loanDetails2 = await SocialLendingContract.loanDetails(2);
