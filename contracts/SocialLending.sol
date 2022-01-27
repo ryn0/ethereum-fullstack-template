cdab49f70f65f5e7b5febd7be661b22979c725d3
Ryan Williams <ryanmichaelwilliams@gmail.com>
2022-01-27 13:11:33 -0700

    detail loan in comments

--- contracts/SocialLending.sol
+++ contracts/SocialLending.sol
@@ -20,8 +20,8 @@ contract SocialLending {
         uint256 loanID,
         uint256 tenor,
         uint128 loanAmount,
-        uint128 amountDeposited,
-        uint128 amountRepaid,
+        uint128 amountDeposited, // the amount depoisited by lenders to the loan 
+        uint128 amountRepaid, // the amount the borrower has repaid to the loan
         uint128 interestRate,
         address borrowerAddress,
         uint128 loanAmountWithInterest,
